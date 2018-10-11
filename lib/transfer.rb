4dcc9792ad54e86529dbdabde08536bafceabb64
Peter Kreppein <pmkreppein@gmail.com>
2018-10-11 19:49:45 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,27 +1,26 @@
 class Transfer
-  # your code here
-  attr_reader :sender, :receiver, :amount 
-  attr_accessor :status
-  
-  def initialize(s, r, a)
-    @sender = s
-    @receiver = r
-    @amount = a
+ attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
     @status = "pending"
   end
-  
+
   def valid?
-    receiver.valid? && sender.valid?
+    self.receiver.valid? && self.sender.valid? && self.sender.balance > self.amount
   end
-  
+
   def execute_transaction
-    if self.status != "complete" && valid? 
+    if self.status != "complete" && self.valid?
       #needs to check if transaction happened once already (stat = complete)
         self.sender.balance -= self.amount
         self.receiver.balance += self.amount
         self.status = "complete"
       else
-        reject_transfer
+        self.status = "rejected"
+        return "Transaction rejected. Please check your account balance."
       end
   end
 
@@ -32,10 +31,7 @@ class Transfer
       self.status = "reversed"
     end
   end
-   def reject_transfer
-    self.status = "rejected"
-    "Transaction rejected. Please check your account balance."
-  end
-      
 
-end #clsend
+
+
+end
