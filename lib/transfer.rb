2546a946385b5538ccd5547a54443821a085d7a8
Peter Kreppein <pmkreppein@gmail.com>
2018-10-11 19:35:24 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,4 +12,16 @@ class Transfer
   def valid?
     self.receiver.valid? && self.sender.valid?
   end
+  
+  def execute_transfer
+    
 end
+
+def transfer_mechanics
+    self.sender.balance -= self.amount
+    self.receiver.balance += self.balance
+    self.status == complete
+end
+    
+
+end #clsend
