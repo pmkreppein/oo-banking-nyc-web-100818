fc00039b3295aed475edb029b16d85820407eb92
Peter Kreppein <pmkreppein@gmail.com>
2018-10-11 19:46:38 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,22 +15,23 @@ class Transfer
   end
   
   def execute_transaction
-    if valid? && sender.balance > amount && self.status == "pending"
-      sender.balance -= amount
-      receiver.balance += amount
-      self.status = "complete"
-    else
-      reject_transfer
-    end
+    if self.status != "complete" && self.valid?
+      #needs to check if transaction happened once already (stat = complete)
+        self.sender.balance -= self.amount
+        self.receiver.balance += self.amount
+        self.status = "complete"
+      else
+        self.status = "rejected"
+        return "Transaction rejected. Please check your account balance."
+      end
   end
-  
-  def reject_transfer
-    self.status = "rejected"
-    "Transaction rejected. Please check your account balance."
-  end
-  
+
   def reverse_transfer
-    
+    if self.status == "complete"
+      self.sender.balance += self.amount
+      self.receiver.balance -= self.amount
+      self.status = "reversed"
+    end
   end
    
       
